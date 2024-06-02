aece4e60e9ecd46b6e22aaf86f0695731c87686d
KennethM19 <ken.jav550@gmail.com>
2024-06-01 22:01:49 -0500

    fix: add data-tracks

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover: 'https://lastfm.freetls.fastly.net/i/u/300x300/1a1cc9431ffacc1b7be877d61975dfc8.jpg',
     album: 'Gioly & Assia',
-    name: 'BEBE (Oficial)'
+    name: 'BEBE (Oficial)',
+    url: 'http://localhost/track.mp3',
+    _id: 1
   }
   constructor() { }
   ngOnInit(): void {
