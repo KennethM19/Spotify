2b3b58eb514cf6c06cd75b20087baffd55a1cf86
KennethM19 <ken.jav550@gmail.com>
2024-06-06 22:36:46 -0500

    fix: add error session conected  message

--- Spotify/src/app/modules/auth/pages/login-page/login-page.component.ts
+++ Spotify/src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -5,28 +5,37 @@ import { AuthService } from '@modules/auth/services/auth.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
-  constructor(private authService: AuthService){}
-  ngOnInit(): void{
+  loginError: string | null = null;
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {
     this.formLogin = new FormGroup({
-      email: new FormControl('',[
-        Validators.required,
-        Validators.email
-      ]),
-      password: new FormControl('',[
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
         Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(12)
-      ])
+        Validators.maxLength(12),
+      ]),
     });
   }
 
   sendLogin(): void {
-    const {email,password} = this.formLogin.value
-    this.authService.sendCredentials(email, password)
+    const { email, password } = this.formLogin.value;
+    this.authService.sendCredentials(email, password).subscribe(
+      {
+        next: (response) => {
+          console.log('Sesión iniciada correctamente');
+          this.loginError = null;
+        }, 
+        error: (error) => {
+          console.log('Error en la contraseña o usuario');
+          this.loginError = 'Error en la contraseña o usuario';
+        },
+        complete: () => console.log('Completado'),
+      }
+    );
   }
-
 }
