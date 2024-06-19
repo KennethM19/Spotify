9f4bacbd1b0dafa5c93a9171750e7b2166680db1
KennethM19 <ken.jav550@gmail.com>
2024-06-19 00:17:21 -0500

    fix: show results search

--- Spotify/src/app/modules/history/pages/history-page/history-page.component.ts
+++ Spotify/src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { SearchService } from '@modules/history/services/search.service';
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
-  styleUrl: './history-page.component.css'
+  styleUrl: './history-page.component.css',
 })
 export class HistoryPageComponent implements OnInit {
-  constructor() {}
+  listResults: TrackModel[]=[]
+  constructor(private searchService: SearchService) {}
   ngOnInit(): void {}
-  receiveData(event: string): void{
-    console.log('👍estoy en el padre -->', event)
-    }
-    
+  receiveData(event: string): void {
+    console.log('👍estoy en el padre -->', event);
+    this.searchService.searchTracks$(event).subscribe(({data}) =>{
+      this.listResults = data
+      console.log('.....',data)
+    })
+  }
 }
